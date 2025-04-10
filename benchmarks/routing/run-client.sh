67fd662a5535757bcf71bc0ee1e09ea43e0d1c48
“Gangmuk <gangmuk@gmail.com>
2025-04-10 16:37:58 -0700

    update path in run-client
    Signed-off-by: “Gangmuk <gangmuk@gmail.com>

--- benchmarks/routing/run-client.sh
+++ benchmarks/routing/run-client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-aibrix_repo="/Users/bytedance/projects/aibrix-routing-experiment"
-input_workload_path="/Users/bytedance/projects/aibrix-routing-experiment/benchmarks/routing/workload/one_request.jsonl"
+aibrix_repo="/Users/bytedance/projects/aibrix/aibrix-gangmuk-kvcache"
+input_workload_path="/Users/bytedance/projects/aibrix/aibrix-gangmuk-kvcache/benchmarks/routing/workload/one_request.jsonl"
 target_ai_model="deepseek-llm-7b-chat"
 api_key="sk-kFJ12nKsFVfVmGpj3QzX65s4RbN2xJqWzPYCjYu7wT3BlbLi" # set your api key
-output_jsonl_path="/Users/bytedance/projects/aibrix-routing-experiment/benchmarks/routing/output.jsonl"
-routing="prefix-cache-and-load"
+output_jsonl_path="/Users/bytedance/projects/aibrix/aibrix-gangmuk-kvcache/benchmarks/routing/output.jsonl"
+routing="random"
 
 python3 ${aibrix_repo}/benchmarks/client/client.py \
     --workload-path ${input_workload_path} \
